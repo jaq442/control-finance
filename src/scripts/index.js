a9a209e84eaff30a4304429df8dd30d5b3682886
Jaqueline Hirose <jaquelinejhirose@gmail.com>
2023-06-10 15:48:36 -0300

    Entrega

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,189 @@
-/* Desenvolva sua lógica aqui */
+import { insertedValues } from './valuesData.js'
+
+//SOMA
+
+export const createSum = (arraySumTotalValue) => {
+    const sumArea = document.querySelector("#filters-value-add")
+    sumArea.innerHTML = ''
+    const sumTitle = document.createElement('p')
+    const valueContainer = document.createElement('span')
+    const sumCipher = document.createElement('p')
+    const sumTotal = document.createElement('p')
+
+    sumTitle.innerText = "Soma dos valores"
+    valueContainer.id = "filters-value-area"
+    sumCipher.id = "filters-value-target"
+    sumCipher.innerText = "R$"
+    sumTotal.id = "filters-value-total"
+
+    valueContainer.append(sumCipher, sumTotal)
+    sumArea.append(sumTitle, valueContainer)
+
+    const totalValue = arraySumTotalValue.reduce((accumulator, currentValue) => accumulator + currentValue.value, 0);
+
+    const totalContainer = document.getElementById("filters-value-total")
+
+    totalContainer.textContent = totalValue;
+}
+
+//FILTRO
+
+export const filterItems = (array) => {
+    const sumButton = document.querySelectorAll('.filters__btn')
+    const items = document.querySelector('.filters__values') //ul
+    const sumValue = document.querySelector('#filters-value-add')
+
+    renderValue(array)
+    createSum(array)
+    
+    sumButton.forEach(btn => {
+        btn.addEventListener("click", () => {
+            sumValue.innerHTML = ''
+            items.innerHTML = ''
+            if(btn.innerText == "Saídas"){
+            const outFiltered = array.filter(item => item.categoryID == 1)
+            createSum(outFiltered)
+            renderValue(outFiltered)
+            if(document.querySelector(".filters__values").getElementsByTagName("li").length === 0){ // se não tiver li quando clicar no remove button
+                const none = document.querySelector(".filters__values"); //acesso ul
+                
+                none.insertAdjacentHTML("afterbegin", `<li class="none"> 
+                <h2>Nenhum valor cadastrado</h2>
+                <p>Insira um valor</p>
+                </li>`)
+            }
+            
+            }
+                
+            if(btn.innerText == "Entradas"){
+                const inFiltered = array.filter(item => item.categoryID == 0)
+                createSum(inFiltered)
+                renderValue(inFiltered)
+                if(document.querySelector(".filters__values").getElementsByTagName("li").length === 0){ // se não tiver li quando clicar no remove button
+                    const none = document.querySelector(".filters__values"); //acesso ul
+                    
+                    none.insertAdjacentHTML("afterbegin", `<li class="none"> 
+                    <h2>Nenhum valor cadastrado</h2>
+                    <p>Insira um valor</p>
+                    </li>`)
+                }
+            
+            }
+
+            if(btn.innerText == "Todos"){
+                
+                createSum(insertedValues)
+                renderValue(insertedValues)
+                if(document.querySelector(".filters__values").getElementsByTagName("li").length === 0){ // se não tiver li quando clicar no remove button
+                    const none = document.querySelector(".filters__values"); //acesso ul
+                    
+                    none.insertAdjacentHTML("afterbegin", `<li class="none"> 
+                    <h2>Nenhum valor cadastrado</h2>
+                    <p>Insira um valor</p>
+                    </li>`)
+                }
+                
+            }
+        
+            deleteValue(array)
+      
+        })})
+}
+
+
+//CARDS VALORES
+
+export function renderValue (arrayItems) { 
+    const valuesArea = document.querySelector(".filters__values")
+    valuesArea.innerHTML = ''
+
+    arrayItems.forEach((item) => {     
+       const card = createValueCard(item)     
+    })
+
+    deleteValue(arrayItems)
+}
+
+
+function createValueCard (arrayItems) {
+    const valuesArea = document.querySelector(".filters__values")
+
+    const item = document.createElement('li')
+    const valueContainer = document.createElement('span')
+    const cipher = document.createElement('p')
+    const value = document.createElement('p')
+    const inOutContainer = document.createElement('span')
+    const inOutValue = document.createElement('p')
+    const deleteButton = document.createElement('button')
+    const deleteImg = document.createElement('img')
+
+    item.classList.add('filters__item')
+    item.id = arrayItems.id
+    valueContainer.classList.add('filters__container-value')
+    cipher.innerText = "R$"
+    cipher.classList.add('filters__value')
+    value.innerHTML = arrayItems.value
+    value.classList.add('filters__price')
+    inOutContainer.classList.add('filters__value-line')
+    inOutValue.classList.add('filters__in-out')
+    
+    if(arrayItems.categoryID == 0){
+        inOutValue.innerText = "Entrada"
+    } if (arrayItems.categoryID == 1){
+        inOutValue.innerText = "Saída"
+    }
+    
+    deleteButton.classList.add('filters__delete')
+    deleteImg.dataset.valueId = arrayItems.id
+    deleteImg.classList.add('deleteImg')
+    deleteImg.src = "./src/assets/trash.svg"
+    
+    valueContainer.append(cipher, value)
+    deleteButton.appendChild(deleteImg)
+    inOutContainer.append(inOutValue, deleteButton)
+    item.append(valueContainer, inOutContainer)
+    valuesArea.appendChild(item)
+    
+}
+
+
+
+//DELETE
+
+function deleteValue(array) {
+    const deleteBtns = document.querySelectorAll('.deleteImg')
+
+    deleteBtns.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            
+        const valueId = Number(event.target.dataset.valueId) //transformar id do botão em numero
+
+        const index = insertedValues.findIndex(value => { //index do elemento-value
+           return value.id === valueId
+        })
+        
+        array.splice(index, 1)
+        filterItems(array)
+        
+        if(document.querySelector(".filters__values").getElementsByTagName("li").length === 0){ // se não tiver li quando clicar no remove button
+            const none = document.querySelector(".filters__values"); //acesso ul
+            
+            none.insertAdjacentHTML("afterbegin", `<li class="none"> 
+            <h2>Nenhum valor cadastrado</h2>
+            <p>Insira um valor</p>
+            </li>`)
+        }})
+    })
+}
+
+
+filterItems(insertedValues)
+
+
+
+
+
+
+
+
+
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,75 @@
-/* Desenvolva sua lógica aqui */
+import { insertedValues } from "./valuesData.js"
+import { renderValue, createSum, filterItems } from './index.js'
+
+const handleModal = () => {
+    const openModal = document.querySelector('#menu-btn')
+    const modal = document.querySelector(".modal__controller")
+
+    openModal.addEventListener('click', (e) => modal.showModal())
+
+    closeModal ()
+}
+
+
+const closeModal = () => {
+    const closeButton = document.querySelector('.modal__close')
+    const modalClose = document.querySelector('.modal__controller')
+    const cancelButton = document.querySelector('#modal-btn-cancel')
+
+    closeButton.addEventListener('click', (event) => {
+        event.preventDefault()
+        modalClose.close()
+    })
+    cancelButton.addEventListener('click', (event) => {
+        event.preventDefault()
+        modalClose.close()
+    })
+
+}
+
+
+const handleRegisterValue = (array) => {
+    const inputValue = document.querySelector('.modal__value')
+    const insertButton = document.getElementById('modal-btn-insert')
+    const modal = document.querySelector(".modal__controller")
+    
+    
+    insertButton.addEventListener('click', (event) => {
+        let newValue = {}
+       
+        event.preventDefault()
+        
+        newValue.id = array.length + 1
+        
+        
+        newValue[inputValue.name] = Number(inputValue.value)
+
+            const modalIn = document.querySelector('#modal-in')
+            const modalOut = document.querySelector('#modal-out')         
+
+            if(modalIn.checked){
+                newValue.categoryID = 0
+            } if (modalOut.checked) {
+                newValue.categoryID = 1
+            }
+            
+            array.push(newValue)
+
+            console.log(insertedValues)
+            
+            renderValue(array)
+            createSum(array)
+            filterItems(array)
+            
+            inputValue.value = ''
+            modal.close()
+    })
+
+}
+    
+
+handleModal()
+handleRegisterValue(insertedValues)
+    
+    
+
